eb35e3913eb56aae1e21971324b042014bc4458e
Joy Iroegbu <mapt.dubem@gmail.com>
2024-07-21 20:42:04 +0100

    Create 1.2-functions.R

---
+++ 1.0-basic-R/1.2-functions.R
@@ -0,0 +1,38 @@
+# mathematical operations
+5+3
+12/7
+
+# creating R object
+weight_kg <- 100
+weight_kg
+
+# converting kg to pounds
+weight_kg *2.2
+# 121
+weight_lb <- weight_kg * 2.2
+weight_lb
+
+
+# challenge
+mass <- 47.5
+age <- 122
+mass <- mass * 2.0
+mass
+age <- age - 20
+age
+mass_index <- mass/age
+mass_index
+
+# functions
+weight_kg <- sqrt(10)
+weight_kg
+
+?sqrt # getting help on a function
+
+measurement <- -30
+abs(measurement)
+
+# function with multiple arguments
+mass_round <- round(mass_index, 2)
+mass_round
+
