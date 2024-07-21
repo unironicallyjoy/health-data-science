a2d8041b2555ec227a7b93604ccf162fd4e786bd
Joy Iroegbu <mapt.dubem@gmail.com>
2024-07-21 20:24:54 +0100

    add creating R object section

---
+++ 1.0-basic-R/1.1-creating-objects-in-R.R
@@ -0,0 +1,24 @@
+# mathematical operations
+5+3
+12/7
+
+# creating R object
+weight_kg <- 100
+weight_kg
+
+# converting kg to pounds
+weight_kg *2.2
+# 121
+weight_lb <- weight_kg * 2.2
+weight_lb
+
+
+# challenge
+mass <- 47.5
+age <- 122
+mass <- mass * 2.0
+mass
+age <- age - 20
+age
+mass_index <- mass/age
+mass_index
